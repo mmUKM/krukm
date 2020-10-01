e6f875ec549bbfb15dc5d952f8391b1c6efead70
jrajalu <jrajalu@jrs-MacPro.local>
2020-10-01 16:15:10 +0800

    No commit message

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+
+
+
+function_krukm_scripts() {
+
+    wp_enqueue_style( 'uikit', 'https://cdn.jsdelivr.net/npm/uikit@3.5.8/dist/css/uikit.min.css' );
+
+    wp_enqueue_scripts( 'uikit');
+
+}
